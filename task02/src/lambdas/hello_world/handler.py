4427411eec614eb77c8e74d058a294cf3a5492fd
hzt <hzt@tes.com>
2024-05-20 10:38:15 +0200

    changes 12

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,5 +1,6 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
+import json
 
 _LOG = get_logger('HelloWorld-handler')
 
@@ -10,11 +11,25 @@ class HelloWorld(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
-        return 200
+        method = event['requestContext']['http']['method']
+        path = event['rawPath']
+
+
+        if(method=="GET" and path=="/hello"):
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                    'message': 'Hello from Lambda'
+
+                })
+            }
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    "message": f"Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+                })
+            }
     
 
 HANDLER = HelloWorld()
